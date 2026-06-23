33466cb94f0f9bd5f57c453b4d093abd1e913f19
nazarudinf313-cmd <nazarudinf313@students.unnes.ac.id>
2026-06-23 23:12:54 +0700

    Update app.py

--- github-repo-football-xai/github-repo/app.py
+++ github-repo-football-xai/github-repo/app.py
@@ -15,6 +15,7 @@ import joblib
 import shap
 import matplotlib.pyplot as plt
 import os
+st.write(os.listdir("github-repo-football-xai"))
 
 st.write("Current Directory:", os.getcwd())
 st.write("Files:", os.listdir())
