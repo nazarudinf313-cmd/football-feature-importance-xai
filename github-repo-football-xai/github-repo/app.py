8ed3e74240b4a76face0e43335cf48e0f2598dce
nazarudinf313-cmd <nazarudinf313@students.unnes.ac.id>
2026-06-23 23:19:35 +0700

    Update app.py

--- github-repo-football-xai/github-repo/app.py
+++ github-repo-football-xai/github-repo/app.py
@@ -15,8 +15,8 @@ import joblib
 import shap
 import matplotlib.pyplot as plt
 import os
-st.write(os.listdir("github-repo-football-xai"))
 
+st.write(os.listdir("github-repo-football-xai"))
 st.write("Current Directory:", os.getcwd())
 st.write("Files:", os.listdir())
 
@@ -46,9 +46,7 @@ BASE_ATTRS = list(ATTRIBUTE_INFO.keys())
 @st.cache_resource
 def load_model():
     try:
-       bundle = joblib.load(
-    "github-repo-football-xai/github-repo/model.pkl"
-)
+        bundle = joblib.load("model.pkl")
         return bundle["model"], bundle["features"]
 
     except Exception as e:
